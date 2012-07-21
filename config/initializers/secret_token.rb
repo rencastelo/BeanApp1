23cfb283da1cacc3eb7e53a6ef467ad605769ac3
Renato Castelo <rcastelo.i@gmail.com>
2012-07-21 14:17:03 -0700

    Primeiro Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beanapp1::Application.config.secret_token = '810b0ac9e9121a49a8c13d9c4b5bad5f73ec0d71d7a576b6942f71f2b62b986602ad1c3467a9f635c75feb84bdca577adb1f38e58b27bfaa3374f0da16f1ba88'
